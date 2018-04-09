d8133d83a15fcfea8884a3caaae16a1ade987a9e
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 20:52:08 -0500

    Update P4_IfElseMystery.java

--- src/P4_IfElseMystery.java
+++ src/P4_IfElseMystery.java
@@ -1,22 +1,26 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. Should print out empty or a " "
+ 2.  Should also print out empty or a " "
+ 3.  Should print out a 3 then a 4 on the same line
+ 4. Should print out a 29 then a 30 on the same line
   
  */
 public class P4_IfElseMystery {
 
 	public static void main(String[] args) {
-		// Put your call to ifElseMyster2 here
+		//ifElseMystery(10, 2);
+		//ifElseMystery(3, 8);
+		//ifElseMystery(4, 4);
+		//ifElseMystery(10, 30);
+
 
 	}
 	
