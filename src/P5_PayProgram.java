53336ce4cc9a6b114b48c4db999415fa7e6cc953
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 21:00:07 -0500

    Update P5_PayProgram.java

--- src/P5_PayProgram.java
+++ src/P5_PayProgram.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 public class P5_PayProgram {
@@ -17,8 +17,24 @@ public class P5_PayProgram {
 		System.out.println("Enter the number of hours you worked this week: ");
 		int numOfHoursWorked = input.nextInt();
 		
+		System.out.println("Now Computing Pay: ");
 		
+		ComputePay(hourlyRate, numOfHoursWorked);
 
 	}
 
+	
+	
+public static double ComputePay(double x, int y) {
+		
+	//(5.50, 6) should return 33.0	
+	double pay;
+	pay = x*y;
+	
+	System.out.println(pay);
+	return pay;
+	
+	
+	}
+	
 }
